28ccc3ba780c81e523bf8c3e2c936ffaae17d01a
Suhan Muppavaram <suhan.m@hotmail.com>
2019-12-01 20:09:59 +1300

    first commit

---
+++ src/test/java/com/fullstackpractice/practiceapplication/PracticeapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackpractice.practiceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
